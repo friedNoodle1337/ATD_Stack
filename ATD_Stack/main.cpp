f99235b560a24e840d6bc35142dd71acd588648d
Artem Savranenko <savranenko.as@edu.spbstu.ru>
2022-06-06 15:43:48 +0300

    Remove comments. Add some details in output

--- ATD_Stack/main.cpp
+++ ATD_Stack/main.cpp
@@ -5,29 +5,24 @@
 
 int main()
 {
-  //const char* InfixExp = "(1+2-(3+4))*(5-1)/(-2)";
-  //const int InfixExpSize = getSizeOfCString(InfixExp);
-  //char* PostfixExp = new char[InfixExpSize];
-  //try
-  //{
-  //  getPostfixFromInfix(InfixExp, PostfixExp, 12);
-  //  std::cout << evaluatePostfix(PostfixExp, 12) << "\n";
-  //}
-  //catch (const StackArray< char >::StackException& exc)
-  //{
-  //  std::cerr << exc.getMessage();
-  //}
-  //catch (const std::exception& exc)
-  //{
-  //  std::cerr << exc.what();
-  //}
-
-  StackArray< char > stack;
-  stack.push('a');
-  stack.push('b');
-  stack.push('c');
-  stack.push('d');
-  std::cout << stack << "\n";
+  const char* InfixExp = "(1+2-(3+4))*(5-1)/(-2)";
+  std::cout << "Infix expression is " << InfixExp << "\n";
+  const int InfixExpSize = getSizeOfCString(InfixExp);
+  char* PostfixExp = new char[InfixExpSize];
+  try
+  {
+    getPostfixFromInfix(InfixExp, PostfixExp, 12);
+    std::cout << "\nPostfix expression is " << PostfixExp << "\n";
+    std::cout << "\nThis expression equals to " << evaluatePostfix(PostfixExp, 12) << "\n";
+  }
+  catch (const StackArray< char >::StackException& exc)
+  {
+    std::cerr << exc.getMessage();
+  }
+  catch (const std::exception& exc)
+  {
+    std::cerr << exc.what();
+  }
 
   return 0;
 }
